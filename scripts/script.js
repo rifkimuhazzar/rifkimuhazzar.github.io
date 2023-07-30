dacae79046bc835571d61e1f0b4ccaeb87b85d5a
Rifki Muhazzar <rifkiki78@gmail.com>
2023-07-30 16:14:34 +0700

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// Menu Bar for mobile
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
